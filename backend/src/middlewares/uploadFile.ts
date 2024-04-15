0e8da5fa7008b6a865580b6dedb287c4e144c0d4
Carlos Leal <carlos.frleal@gmail.com>
2024-04-15 20:08:32 -0300

    WIP: configuração do multer

---
+++ backend/src/middlewares/uploadFile.ts
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import path from 'path';
+import { Request } from 'express';
+import crypto from 'crypto';
+
+const storage = multer.diskStorage({
+  destination: function (req: Request, file: Express.Multer.File, cb: Function) {
+    cb(null, path.join(__dirname, '../uploads')); // define o local para o arquivo
+  },
+  filename: function (req: Request, file: Express.Multer.File, cb: Function) {
+    const fileExtension = file.originalname.split('.')[1];
+    const newFileName = crypto.randomBytes(16).toString('hex');
+    cb(null, `${newFileName}.${fileExtension}`);
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
+
--- backend/src/routes/ProductRoute.ts
+++ backend/src/routes/ProductRoute.ts
@@ -1,5 +1,6 @@
 import ProductController from '../controllers/ProductController';
 import { Request, Response, Router } from 'express';
+import upload from '../middlewares/uploadFile';
 
 const router = Router();
 
@@ -7,5 +8,10 @@ const productController = new ProductController();
 
 router.get('/', (req: Request, res: Response) =>
   productController.getAllProducts(req, res));
+
+router.post('/update-price', upload.single('file'), (req: Request, res: Response) => {
+  console.log('Arquivo enviado:', req.file?.filename);
+  res.send('Arquivo enviado com sucesso!');
+})
 
 export default router;
